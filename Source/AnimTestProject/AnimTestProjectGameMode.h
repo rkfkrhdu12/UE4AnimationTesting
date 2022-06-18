1b5c4a192a0cd97dcb1a51a779c081969da136a3
rkfkrhdu12 <33976974+rkfkrhdu12@users.noreply.github.com>
2022-06-18 15:12:41 +0900

    BaseFile

---
+++ Source/AnimTestProject/AnimTestProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AnimTestProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAnimTestProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAnimTestProjectGameMode();
+};
+
+
+
